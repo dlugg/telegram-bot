97b2281bbcd4a20636a087d8b0178e12889c4f8f
Andrzej Ługowski <155900687+dlugg@users.noreply.github.com>
2026-08-10 17:41:56 +0200

    refactor: build task list with IntStream

--- src/main/java/commands/ListCommand.java
+++ src/main/java/commands/ListCommand.java
@@ -3,30 +3,31 @@ package commands;
 import service.TaskService;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ListCommand implements Command {
-private final TaskService taskService;
-    public ListCommand(TaskService taskService){
-        this.taskService= taskService;
+    private final TaskService taskService;
+
+    public ListCommand(TaskService taskService) {
+        this.taskService = taskService;
     }
+
     @Override
     public String execute(long chatId, String args) {
         List<String> currentTasks = taskService.getTasks(chatId);
-        StringBuilder currentTasksOutput = new StringBuilder();
         if (currentTasks.isEmpty()) {
-            return "Задачи отсутствуют ";
-        } else {
-            currentTasksOutput.append("Текущие задачи: \n");
-            for (int i = 0; i < currentTasks.size(); i++) {
-                currentTasksOutput.append(i + 1).append(". ").append(currentTasks.get(i)).append("\n");
-
-            }
+            return "Список задач пуст";
         }
-        return currentTasksOutput.toString();
+        String result = "Текущие задачи: \n" + IntStream.range(0, currentTasks.size())
+                .mapToObj(i -> i + 1 + ". " + currentTasks.get(i))
+                .collect(Collectors.joining("\n"));
+        return result;
+
     }
 
     @Override
-    public String description(){
+    public String description() {
         return "показать список задач";
     }
 }
