4a5281873f535f64c2dd3f6728becfc84d56cf57
Andrzej Ługowski <155900687+dlugg@users.noreply.github.com>
2026-09-05 15:42:43 +0200

    refactor: make Database configurable for tests

--- src/main/java/service/Database.java
+++ src/main/java/service/Database.java
@@ -5,11 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
-    private static final String URL = "jdbc:postgresql://localhost:5432/javabot";
-    private static final String USER = "postgres";
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public Database() {
+        this("jdbc:postgresql://localhost:5432/javabot",
+                "postgres",
+                System.getenv("DATABASE_PASSWORD"));
+    }
+
+    public Database(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
