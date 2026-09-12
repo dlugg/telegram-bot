516524fcc5da59ba8bf2292651eeda4cfc1134a6
Andrzej Ługowski <155900687+dlugg@users.noreply.github.com>
2026-09-12 16:07:05 +0200

    chore: add game tables to schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS users
     chat_id BIGINT NOT NULL UNIQUE,
     name    TEXT
 );
+
 CREATE TABLE IF NOT EXISTS tasks
 (
     id         BIGSERIAL PRIMARY KEY,
@@ -11,4 +12,19 @@ CREATE TABLE IF NOT EXISTS tasks
     task_text  TEXT      NOT NULL,
     is_done    BOOLEAN   NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS rps_rounds
+(
+    id        BIGSERIAL PRIMARY KEY,
+    user_id   BIGINT      NOT NULL REFERENCES users (id),
+    result    VARCHAR(32) NOT NULL CHECK ( result in ('WIN', 'LOSE', 'DRAW') ),
+    played_at timestamptz NOT NULL DEFAULT now()
+);
+CREATE TABLE IF NOT EXISTS guess_games
+(
+    id        BIGSERIAL PRIMARY KEY,
+    user_id   BIGINT      NOT NULL REFERENCES users (id),
+    attempts  INT         NOT NULL CHECK ( attempts > 0 ),
+    played_at timestamptz NOT NULL DEFAULT now()
 );
